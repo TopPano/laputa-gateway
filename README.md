bf2b4bee82512941800d00a4cbe944860ae9f277
uniray7 <uniray7@gmail.com>
2016-04-28 08:02:19 +0000

    add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# laputa-gateway
+
+### Setup:
+1. git clone https://github.com/TopPano/laputa-gateway.git
+2. cd laputa-gateway
+3. npm install
+4. vim config.json
+5. npm run-script service 
+
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var http = require('http');
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    config = require('./config.json');
 
 var proxy = httpProxy.createProxyServer({});
+
 http.createServer(function(req, res) {
     
     proxy.on("error", function(e){console.log(e);});
-
     if(req.headers['user-agent.match'] !== 'ELB-HealthChecker\/1.0')
     {
         if(req.headers['x-forwarded-proto']=== 'http'){
@@ -19,14 +20,14 @@ http.createServer(function(req, res) {
             if(req.url.match(/\/api\/(.*)/g)) 
             {
                 proxy.web(req, res, 
-                          { target: 'http://verpix-api-elb-1183746318.ap-northeast-1.elb.amazonaws.com/' }, 
+                          { target: config.apiProxy }, 
                           function(e){console.log(e)});
             }
             else
             {
-                proxy.web(req, res, { target: 'http://localhost:3001' },
+                proxy.web(req, res, { target: config.viewerProxy},
                          function(e){console.log(e)});
             }
         }
     }
-}).listen(9999);
+}).listen(config.port);
